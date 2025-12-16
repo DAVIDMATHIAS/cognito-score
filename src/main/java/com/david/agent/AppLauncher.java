7635cdc16118da5728c36ff7bf01c886660f56f2
davidmathias <thestardavid@gmail.com>
2025-12-17 02:34:05 +0530

    refactoring

--- src/main/java/com/david/agent/AppLauncher.java
+++ src/main/java/com/david/agent/AppLauncher.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.embabel.template;
+package com.david.agent;
 
 import com.embabel.agent.config.annotation.EnableAgents;
 import com.embabel.agent.config.annotation.LoggingThemes;
@@ -23,8 +23,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableAgents(loggingTheme = LoggingThemes.STAR_WARS)
-class ProjectNameApplication {
+class AppLauncher {
     public static void main(String[] args) {
-        SpringApplication.run(ProjectNameApplication.class, args);
+        SpringApplication.run(AppLauncher.class, args);
     }
 }
